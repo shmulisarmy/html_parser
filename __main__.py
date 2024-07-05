e2dab83f2edeeded2ca03837bf6c2d69816e60e5
shmuli <shmulikeller@gmail.com>
2024-07-05 15:45:37 -0400

    removed_some_more_prints

--- __main__.py
+++ __main__.py
@@ -19,51 +19,30 @@ html_string = (
     "</body>"
 )
 
-print(f"{html_string = }")
-
-
-
 
 
 
 html = HTML.get_html_tag_list(html_string)
 
-print(html)
 
 document: DomTree = DomTree.create_document_from(html)
 
 
 DomTree.all_nodes_in_order = [child for child in document.breadth_first_search_child_generator()]
 
-print(document.atributes)
-
-
-
-div = document.search_for_element(tag_name='div', class_name='fuck')
-another_div = document.search_for_elements(tag_name='div')[-1]
-
-
-print(f"{div = }")
-print(f"{another_div = }")
-
-
-print(div.create_template())
-
-bcs = div.best_common_selector(another_div)
-
-print(f"{bcs = }")
-
-
-matching_text_elements: list[list[DomTree]] = document.find_by_text("hellofuck")
 
 
 
 
 
 
-tested_selector = document.querySelector('body').search_for_elements(tag_name = 'div')
 
-print(f"{tested_selector = }")
+
+
+
+
+
+
 
 
 
